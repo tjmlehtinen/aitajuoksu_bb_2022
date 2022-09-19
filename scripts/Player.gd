c008045e56546fb6ffbdaa6d2285b5a3a27a9a38
Your Name <you@example.com>
2022-09-19 19:29:22 +0300

    sivulle liikkuu

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,19 +1,22 @@
 extends KinematicBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# variables for moving
+var move_speed : float = 10.0
+var side_speed : float = 6.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _physics_process(delta):
-	translation.z -= 3.0 * delta
+	var sideways : int = 0
+	if Input.is_action_pressed("move_left"):
+		sideways = -1
+	if Input.is_action_pressed("move_right"):
+		sideways = 1
+	translation.z -= move_speed * delta
+	translation.x += sideways * side_speed * delta

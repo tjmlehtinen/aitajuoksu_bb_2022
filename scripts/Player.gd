43a8ddeabb17cb1818f1d68c6ed5ee420a2190b1
Your Name <you@example.com>
2022-09-26 19:35:54 +0300

    collidereja ja hyppy

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,6 +3,9 @@ extends KinematicBody
 # variables for moving
 var move_speed : float = 10.0
 var side_speed : float = 6.0
+var velocity : Vector3 = Vector3()
+var jump_force : float = 40.0
+var gravity : float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,5 +21,9 @@ func _physics_process(delta):
 		sideways = -1
 	if Input.is_action_pressed("move_right"):
 		sideways = 1
-	translation.z -= move_speed * delta
-	translation.x += sideways * side_speed * delta
+	if Input.is_action_just_pressed("jump"):
+		velocity.y += jump_force
+	velocity.z = -move_speed
+	velocity.x = sideways * side_speed
+	velocity.y -= gravity
+	velocity = move_and_slide(velocity)
